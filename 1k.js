8fa5ac108724c322515afce595a53ff6ac855de1
Andrew Malone <andrew_malone@harvard.edu>
2014-03-10 16:51:51 -0400

    Created basic circle drawing animated method

--- 1k.js
+++ 1k.js
@@ -1,34 +1,31 @@
-// remove the canvas
-d();
+var circles = [];
 
-ctx = new AudioContext();
-freq = 200
-sample = ctx.sampleRate / freq
-buffer = ctx.createBuffer(1, ctx.sampleRate, ctx.sampleRate)
-data = buffer.getChannelData(0)
-
-for (var i = 0; i < ctx.sampleRate; i++)
-{
-    // generate audio here
-    phase = (i % sample) / sample
-    data[i] = Math.sin(freq * i)
-}
-
-
-// add a button
-btn = document.createElement("button")
-btn.innerHTML = "button"
-document.body.appendChild(btn)
-
-// make something happen when the button is clicked
-btn.addEventListener("click", function() {
-    node = ctx.createBufferSource()
-    node.buffer = buffer
-    node.connect(ctx.destination)
-    //node.playbackRate.value = .5
-    //node.loop = true
-    node.start(ctx.currentTime)
-    //node.stop(ctx.currentTime + .3)
-    //node.playbackRate.setValueAtTime(3, ctx.currentTime)
-    //node.playbackRate.linearRampToValueAtTime(1, ctx.currentTime + .08)
+a.addEventListener("click", function(e) {
+    circles.push({
+        x:e.pageX,
+        y:e.pageY,
+        r:1
+    })
 })
+draw()
+function draw()
+{
+    requestAnimationFrame(draw)
+    // clear the canvas
+    a.width = a.width
+
+    // draw the circles
+    for (var i = 0, n = circles.length; i < n; i++)
+    {
+        if (!circles[i]) break;
+        c.beginPath();
+        c.arc(circles[i].x, circles[i].y, circles[i].r, 0, Math.PI * 2, false)
+        c.stroke()
+        circles[i].r++
+        if (circles[i].r == 50)
+        {
+            circles.splice(i, 1)
+            i--
+        }
+    }
+}
